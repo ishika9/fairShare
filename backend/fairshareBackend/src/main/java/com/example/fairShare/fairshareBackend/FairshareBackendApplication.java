d0cd876648f55433b73dde1f67fc2fef895b3d8e
Ishika Sunil <ishika9sunil@gmail.com>
2025-09-15 22:31:17 +0530

    frontend and backend setup

---
+++ backend/fairshareBackend/src/main/java/com/example/fairShare/fairshareBackend/FairshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fairShare.fairshareBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairshareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairshareBackendApplication.class, args);
+	}
+
+}
